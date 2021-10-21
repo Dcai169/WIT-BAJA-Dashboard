540d6625b602d6c927b41476a41729dde1711158
Daniel Cai <legoguy145@gmail.com>
2021-10-21 15:01:38 -0400

    Added test mode indicator

--- scripts.js
+++ scripts.js
@@ -9,8 +9,9 @@ const TimePreviousElement = document.getElementById("previous");
 const TimeElapsedElement = document.getElementById("elapsed");
 
 const FuelGaugeElement = document.getElementById("fuel-gauge");
+// const FuelTextElement = document.getElementById("fuel-text-container");
 const FuelPercentElement = document.getElementById("fuel-percent");
-const FuelEstimateElement = document.getElementById("fuel-estimate");
+const FuelVolumeElement = document.getElementById("fuel-volume");
 
 const RealDateElement = document.getElementById("real-date");
 const RealTimeElement = document.getElementById("real-time");
@@ -19,74 +20,83 @@ let dateObj = null;
 let apiData = null;
 
 function replaceText(element, text) {
-    if (element) {
-        element.innerText = text;
-    }
+  if (element) {
+    element.innerText = text;
+  }
 }
 
 function setIndicatorState(element, state) {
-    if (element) {
-        element.style.color = state ? 'white' : 'gray'
-    }
+  if (element) {
+    element.style.color = state ? 'white' : 'gray'
+  }
 }
 
 function setField(selector, value) {
-    switch (selector) {
-      case 'fuel-percent':
-        replaceText(FuelPercentElement, `${value}%`);
-        FuelGaugeElement.style.height = `${value}%`;
-        FuelGaugeElement.style.backgroundColor = `hsl(${1.2*parseInt(value)}, 80%, 50%)`
-        break;
-  
-      case 'fuel-estimate':
-        replaceText(FuelEstimateElement, `${value}min`);
-        break;
-  
-      case 'speed':
-        replaceText(SpeedElement, `${value}km/h`);
-        break;
-  
-      case 'heading':
-        replaceText(HeadingElement, value.toString().padStart(3, '0'));
-        break;
-  
-      case 'start-mode':
-        setIndicatorState(StartModeElement, !!value);
-        break;
-  
-      case 'diff-lock':
-        setIndicatorState(DiffLockElement, !!value);
-        break;
-    
-      default:
-        break;
-    }
+  switch (selector) {
+    case 'fuel-percent':
+      replaceText(FuelPercentElement, `${value}%`);
+      FuelGaugeElement.style.height = `${value}%`;
+      FuelGaugeElement.style.backgroundColor = `hsl(${1.2 * parseInt(value)}, 80%, 50%)`
+      // FuelTextElement.style.color = `hsl(${1.2 * parseInt(value) + 270}, 80%, 50%)`
+      break;
+
+    case 'fuel-volume':
+      replaceText(FuelVolumeElement, `${value}mL`);
+      break;
+
+    case 'speed':
+      replaceText(SpeedElement, `${value}kts`);
+      break;
+
+    case 'heading':
+      replaceText(HeadingElement, value.toString().padStart(3, '0'));
+      break;
+
+    case 'start-mode':
+      setIndicatorState(StartModeElement, !!value);
+      break;
+
+    case 'diff-lock':
+      setIndicatorState(DiffLockElement, !!value);
+      break;
+
+    default:
+      break;
   }
+}
 
 async function run_update() {
-    dateObj = new Date();
-    replaceText(RealTimeElement, dateObj.toTimeString().split(' ')[0]);
-    
-    try {
-        apiData = await (await fetch('/api/v1/systems/all')).json()
-    } catch (error) {
-        
-    }
+  dateObj = new Date();
+  replaceText(RealTimeElement, dateObj.toTimeString().split(' ')[0]);
 
-    if (apiData) {
-        setField('fuel-percent', apiData.fuel.level);
-        setField('fuel-estimate', apiData.fuel.estimate);
+  try {
+    apiData = await (await fetch('/api/v1/systems/all')).json()
+  } catch (error) {
 
-        setField('speed', apiData.gps.speed);
-        setField('heading', apiData.gps.heading);
+  }
 
-        setField('start-mode', apiData.other.start_mode);
-        setField('diff-lock', apiData.other.diff_lock);
-    }
-    console.log(`Update Time: ${new Date() - dateObj}ms`)
+  if (apiData) {
+    setField('fuel-percent', apiData.fuel.level);
+    setField('fuel-volume', apiData.fuel.volume);
+
+    setField('speed', apiData.gps.speed);
+    setField('heading', apiData.gps.heading);
+
+    setField('start-mode', apiData.other.start_mode);
+    setField('diff-lock', apiData.other.diff_lock);
+  }
+  console.log(`Update Time: ${new Date() - dateObj}ms`)
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-    replaceText(RealDateElement, new Date().toLocaleDateString());
-    setInterval(run_update, 1000 / 10);
+  replaceText(RealDateElement, new Date().toLocaleDateString());
+
+  fetch('/api/v1/status').then(res => {
+    if (JSON.parse(res).mode == 'test') {
+      document.getElementById('test-mode').style.color = 'white';
+    }
+  })
+  
+  setInterval(run_update, 1000 / 10);
+
 });
