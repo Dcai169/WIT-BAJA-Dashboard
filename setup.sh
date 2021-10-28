62d918c4a62ad649a464c161f7349f38e32a8360
Daniel Cai <legoguy145@gmail.com>
2021-10-28 11:01:37 -0400

    added start and setup scripts

---
+++ setup.sh
@@ -0,0 +1,4 @@
+apt update & apt upgrade -y
+apt install -y midori
+pip install --upgrade pip
+pip install -r requirements.txt
---
+++ start.sh
@@ -0,0 +1,4 @@
+export FLASK_APP=app
+export FLASK_ENV=development
+flask run & sleep 1
+midori -e Fullscreen http://localhost:5000/
